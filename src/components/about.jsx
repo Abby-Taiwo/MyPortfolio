cbd6e1bbc1df04c7da8cd29b229cf0664ec48453
abby-taiwo <abidemitaiwo95@gmail.com>
2022-09-22 03:48:45 -0700

    second commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,8 +7,8 @@ function about() {
       <h4 className='text-lg'>Hi there,</h4>
       <h1 className='text-6xl'>I'm Abby Taiwo</h1>
     </div>
-    <div className='flex-col border md:flex justify-between p-16 mt-20 text-white'>
-      <h4 className='text-lg w-80 mt-20'>I'm a front end developer that hopes to work
+    <div className='grid grid-cols-1 md:grid-cols-2 mt-10 px-8 text-white'>
+      <h4 className='md:text-center text-lg mt-20'>I'm a front end developer that hopes to work
       with teams around the world to help create 
       interactive and outstanding websites, My interest
       in this great skill developed in 2022 and it has 
@@ -16,7 +16,14 @@ function about() {
       and interactive websites that focus on providing the 
       best experience for everyone using them.
       </h4>
-      <img src={mypicture} alt="myslef" width={450} />
+      <div className='flex justify-end items-center md:mt-8 '>
+        <img
+          src={mypicture} 
+          alt="myself" 
+          width={350}
+          className='rounded-md '
+        />
+     </div>
     </div>
   </div>
   )
