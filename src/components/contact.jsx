b5d94cf0cced3a7afaed40bd7cf5995ddd7fe666
abby-taiwo <abidemitaiwo95@gmail.com>
2022-09-30 05:03:27 -0700

    5th commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,7 +5,7 @@ function contact() {
     <div id='contact' className='mb-8 text-white'>  
         <h1 className='text-center mt-20 text-4xl text-bolder '>Contact Me</h1> 
         <div className='w-1/2 mx-auto mt-8 text-center'>
-          <a href="mailto:abidemitaiwo95@gmail.com" className='p-4 rounded-md hover:opacity-80 bg-red-900'>
+          <a href="mailto:abidemitaiwo95@gmail.com" className='p-4 rounded-md hover:opacity-80 bg-purple'>
             <button>
               Say Hello
             </button>
